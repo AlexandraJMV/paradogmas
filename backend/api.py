e33131716e4b2f06540e6dce031cff983e6d4e73
AlexandraJMV <alexandra.mendoza.v@mail.pucv.cl>
2023-11-10 13:06:39 -0300

    link dataset en coment api.py

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,5 @@
-#https://www.kaggle.com/datasets/joebeachcapital/30000-spotify-songs
+#https://www.kaggle.com/datasets/joebeachcapital/30000-spotify-song
+from flask import Flask, request, jsonify
+from waitress import serve
+from flask_cors import CORS
+import json
