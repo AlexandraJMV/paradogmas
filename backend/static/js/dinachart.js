cc60fc1cf9e55093021b4b9aa1bedec7de460966
AlexandraJMV <alexandra.mendoza.v@mail.pucv.cl>
2023-11-27 20:50:55 -0300

    form display : flex

--- backend/static/js/dinachart.js
+++ backend/static/js/dinachart.js
@@ -12,6 +12,17 @@ function updateChart() {
         myChart.destroy();
     }
 
+    let colors = generateRandomColorsArray(data.data.length, function(){ 
+        const hue = (Math.random() < 0.5 ? Math.random() * 20 : 320 + Math.random() * 60);
+
+            const saturation = Math.floor(Math.random() * 70) + 40;
+            const lightness = Math.floor(Math.random() * 50) + 20; // Adjust as needed for a darker or lighter appearance
+      
+            const pastelColor = `hsl(${hue}, ${saturation}%, ${lightness}%, 0.5)`;
+          
+            return pastelColor;
+          })
+
     // Create a new chart with the received data
     let ctx = document.getElementById('myChart').getContext('2d');
     myChart = new Chart(ctx, {
@@ -21,8 +32,8 @@ function updateChart() {
             datasets: [{
                 label: document.getElementById('dataSelect').value,
                 data: data.data,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: colors,
+                borderColor: 'rgba(120, 70, 70, 0.7)',
                 borderWidth: 1
             }]
         },
@@ -62,8 +73,8 @@ let myChart = new Chart(ctx, {
         datasets: [{
             label: 'Selected Data',
             data: [],
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(200, 70, 40, 0.7)',
+            borderColor: 'rgba(200, 70, 40, 0.7)',
             borderWidth: 1
         }]
     },
